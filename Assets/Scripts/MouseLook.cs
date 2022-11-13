b40833b8ecc163ea1821ca05f649106887f417de
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2022-11-13 18:55:46 +0100

    Update sensitivity

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField] private float sensitivity = 100f;
+    [SerializeField] private float sensitivity = 1f;
     [SerializeField] private Transform playerBody;
     [SerializeField] private GameObject weaponParent;
     [SerializeField] private float weaponMovement = 20f;
@@ -12,23 +12,27 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         // Get the mouse input
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
 
         // Set boundaries for the camera
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        // Only in-game (not in the menus)
+        if (Time.timeScale == 1)
+        {
+            // Rotate the camera
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+            // Rotate the player
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
 
         // Smoothly rotate the weapon along the mouseX input
         // Only execute in Build
-        if (Application.isEditor == false)
-        {
         Quaternion desiredRotation = Quaternion.Euler(mouseX * weaponMovement * .5f, 90 - mouseX * weaponMovement, mouseY * weaponMovement);
         weaponParent.transform.localRotation = Quaternion.Lerp(weaponParent.transform.localRotation, desiredRotation, .03f);
-        }
     }
 
     private void OnDrawGizmos()
